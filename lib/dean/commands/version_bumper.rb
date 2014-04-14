1166e71c97de3132be5dbeacea2b101013f7c79e
Gio <giovanni.lodi42@gmail.com>
2014-04-14 23:33:47 +0100

    Implemented minor, patch, and pre version bump method

--- lib/dean/commands/version_bumper.rb
+++ lib/dean/commands/version_bumper.rb
@@ -5,10 +5,19 @@ module Dean
       version = Dean::ProjectVersionHelper.new.version_from_plist plist
       semver_helper = Dean::SemverHelper.new
 
+      new_version = version
+
       if value == :major
         new_version = semver_helper.bump_major version
-        Dean::ProjectVersionHelper.new.set_version_in_plist new_version, plist
+      elsif value == :minor
+        new_version = semver_helper.bump_minor version
+      elsif value == :patch
+        new_version = semver_helper.bump_patch version
+      elsif value == :pre
+        new_version = semver_helper.bump_pre version
       end
+
+      Dean::ProjectVersionHelper.new.set_version_in_plist new_version, plist
     end
   end
 end
--- spec/version_bumper_spec.rb
+++ spec/version_bumper_spec.rb
@@ -5,25 +5,45 @@ describe Dean::VersionBumper do
   let (:bumper) { described_class.new }
 
   describe "#bump" do
+    before(:each) do
+      @temp_plist = setup_plist
+    end
+
     it "should have two mandatory parameter" do
       expect{ bumper.bump }.to raise_error(ArgumentError)
       expect{ bumper.bump "something" }.to raise_error(ArgumentError)
     end
 
     it "should bump the major value of the given version when passed the :major parameter" do
-        temp_plist = Tempfile.new 'plist'
-        plist = {
-          'CFBundleVersion' => '0.0.0',
-          'CFBundleShortVersionString' => '4'
-        }
-        # I don't like using plist gem in here, need more context independency
-        Plist::Emit::save_plist plist, temp_plist.path
-
-        bumper.bump temp_plist.path, :major
-
-        expect(Plist::parse_xml(temp_plist.path)['CFBundleVersion']).to eq '1.0.0'
-
-        temp_plist.unlink
+      bumper.bump @temp_plist.path, :major
+      expect(Plist::parse_xml(@temp_plist.path)['CFBundleVersion']).to eq '1.0.0'
     end
+
+    it "should bump the minor value of the given version when passed the :minor parameter" do
+      bumper.bump @temp_plist.path, :minor
+      expect(Plist::parse_xml(@temp_plist.path)['CFBundleVersion']).to eq '0.1.0'
+    end
+
+    it "should bump the patch value of the given version when passed the :patch parameter" do
+      bumper.bump @temp_plist.path, :patch
+      expect(Plist::parse_xml(@temp_plist.path)['CFBundleVersion']).to eq '0.0.1'
+    end
+
+    it "should bump the pre value of the given version when passed the :pre parameter" do
+      tempfile = setup_plist '0.0.0-pre.0'
+      bumper.bump tempfile.path, :pre
+      expect(Plist::parse_xml(tempfile.path)['CFBundleVersion']).to eq '0.0.0-pre.1'
+    end
+  end
+
+  def setup_plist(version='0.0.0')
+    tempfile = Tempfile.new 'plist'
+    plist = {
+      'CFBundleVersion' => version,
+      'CFBundleShortVersionString' => '4'
+    }
+    # I don't like using plist gem in here, need more context independency
+    Plist::Emit::save_plist plist, tempfile.path
+    return tempfile
   end
 end
