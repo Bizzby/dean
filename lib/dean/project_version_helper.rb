57015486dfc92f6f724e0e62455810c51617990c
Gio <giovanni.lodi42@gmail.com>
2014-04-14 17:57:19 +0100

    Project version helper can set version in plist file

--- lib/dean/project_version_helper.rb
+++ lib/dean/project_version_helper.rb
@@ -3,7 +3,7 @@ module Dean
   class ProjectVersionHelper
 
     def version_from_plist(plist)
-      plist_path = "#{Dir.pwd}/#{plist}"
+      plist_path = File.expand_path(plist, __FILE__)
 
       if not File.exists? plist_path
         puts "Cannot open #{plist_path}. File not found"
@@ -12,6 +12,19 @@ module Dean
 
       plist = Plist::parse_xml plist_path
       version = plist["CFBundleVersion"]
+    end
+
+    def set_version_in_plist(version, plist)
+      plist_path = File.expand_path(plist, __FILE__)
+
+      if not File.exists? plist_path
+        puts "Cannot open #{plist_path}. File not found"
+        return
+      end
+
+      plist = Plist::parse_xml plist_path
+      plist["CFBundleVersion"] = version
+      plist.save_plist plist_path
     end
   end
 end
--- spec/project_version_helper_spec.rb
+++ spec/project_version_helper_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require "tempfile"
 
 describe Dean::ProjectVersionHelper do
   describe "#version_from_plist" do
@@ -6,17 +7,27 @@ describe Dean::ProjectVersionHelper do
 
     context "when the plist exists on disk" do
       before do
-        @expected_version = '4.2.0' 
-        File.stub(:exists?).and_return true
-        # I don't like stubbing this gem. It's not really a black box approach
-        Plist.stub(:parse_xml).and_return({ 
-          'CFBundleVersion' => @expected_version, 
-          'CFBundleShortVersionString' => '4' 
-          })
+        @expected_version = '4.2.0'
+        @temp_plist = Tempfile.new 'plist'
+        @plist = {
+          'CFBundleVersion' => @expected_version,
+          'CFBundleShortVersionString' => '4'
+        }
+        # I don't like using plist gem in here, need more context independency
+        Plist::Emit::save_plist @plist, @temp_plist.path
       end
 
       it "should return the CFBundleVersion value" do
-        expect(helper.version_from_plist 'plist').to eq @expected_version
+        expect(helper.version_from_plist @temp_plist.path).to eq @expected_version
+      end
+
+      it "should set the CFBundleVersion value to the given one" do
+        expected_version = '1.2.3'
+        helper.set_version_in_plist expected_version, @temp_plist.path
+
+        version = Plist::parse_xml(@temp_plist.path)['CFBundleVersion']
+
+        expect(version).to eq expected_version
       end
     end
 
