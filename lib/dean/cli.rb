00f16c22d0ccb6e8ff350ca65cfe892d272038b2
Gio <giovanni.lodi42@gmail.com>
2014-04-24 17:47:50 +0100

    Added a very raw git helper
    Nothing more that the copy of the function we were using in the Rake task

--- lib/dean/cli.rb
+++ lib/dean/cli.rb
@@ -6,7 +6,18 @@ module Dean
 
       desc 'bump_version VALUE', 'Bump the given version VALUE of the project'
       def bump(value)
+        plists = []
+        Dean::ConfigurationHelper.new.all_environments.each do |environment|
+          plists.push environment[:plist]
+        end
+        old_version = Dean::ProjectVersionHelper.new.version_from_plist plists[0]
+
         Dean::VersionBumper.new.bump_all_environments value.to_sym
+
+        # commit and tag, here only for the moment!
+        new_version = Dean::ProjectVersionHelper.new.version_from_plist plists[0]
+        message = "Do you want to commit the changes to the version (#{old_version} -> #{new_version})?"
+        Dean::GitHelper.new.commit_asking_user(message, new_version, plists)
       end
 
       desc 'build', 'Builds the ipa'
