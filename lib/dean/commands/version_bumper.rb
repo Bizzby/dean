ee4a7c5817d0c967149af6dc612424202bd9e1a1
Gio <giovanni.lodi42@gmail.com>
2014-05-09 15:13:01 +0100

    Added method to bump short version to version_bumper

--- lib/dean/commands/version_bumper.rb
+++ lib/dean/commands/version_bumper.rb
@@ -30,5 +30,25 @@ module Dean
 
       Dean::ProjectVersionHelper.new.set_version_in_plist new_version, plist
     end
+
+    def bump_short(plist, value)
+      version = Dean::ProjectVersionHelper.new.short_version_from_plist plist
+      semver_helper = Dean::SemverHelper.new
+
+      new_version = version
+
+      if value == :major
+        new_version = semver_helper.bump_major version
+      elsif value == :minor
+        new_version = semver_helper.bump_minor version
+      elsif value == :patch
+        new_version = semver_helper.bump_patch version
+      elsif value == :pre
+        new_version = semver_helper.bump_pre version
+      end
+
+      Dean::ProjectVersionHelper.new.set_short_version_in_plist new_version, plist
+    end
+
   end
 end
