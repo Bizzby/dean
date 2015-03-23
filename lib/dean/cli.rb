86ef911516fcbddf8f2ac4ef1345d1ad8e8c1a9e
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-03-24 08:44:50 +1100

    Update cli to use xcbump + add '-c' option to commit bump

--- lib/dean/cli.rb
+++ lib/dean/cli.rb
@@ -5,19 +5,23 @@ module Dean
     class Application < Thor
 
       desc 'bump_version VALUE', 'Bump the given version VALUE of the project'
+      method_option :commit, aliases: '-c', desc: 'Commit version bump'
       def bump(value, name=nil)
+        project_version_helper = XCBump::ProjectVersionHelper.new
+
         plists = []
         Dean::ConfigurationHelper.new.all_environments.each do |environment|
           plists.push environment[:plist]
         end
-        old_version = Dean::ProjectVersionHelper.new.version_from_plist plists[0]
+        old_version = project_version_helper.version_from_plist plists[0]
 
         Dean::VersionBumper.new.bump_all_environments value.to_sym, name
 
+        return unless options[:commit]
         # commit and tag, here only for the moment!
-        new_version = Dean::ProjectVersionHelper.new.version_from_plist plists[0]
+        new_version = project_version_helper.version_from_plist plists[0]
         message = "Do you want to commit the changes to the version (#{old_version} -> #{new_version})?"
-        Dean::GitHelper.new.commit_asking_user(message, new_version, plists)
+        XCBump::GitHelper.new.commit_asking_user(message, new_version, plists)
       end
 
       desc 'build', 'Builds the ipa'
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,38 +1,72 @@
 require 'spec_helper.rb'
 require 'tempfile'
+require 'yaml'
 
-describe Dean::Cli do
+def stub_dean_yml(plist_path)
+  @deanfile = File.new(Dir.pwd + '/.dean.yml', 'w')
+  config = {
+    environments: [
+      {
+        plist: plist_path
+      }
+    ]
+  }
+  @deanfile.write(config.to_yaml)
+  @deanfile.close
+end
+
+def capture_stdout(&block)
+  original_stdout = $stdout
+  $stdout = fake = StringIO.new
+  begin
+    yield
+  ensure
+    $stdout = original_stdout
+  end
+  fake.string
+end
+
+def capture_stderr(&block)
+  original_stderr = $stderr
+  $stderr = fake = StringIO.new
+  begin
+    yield
+  ensure
+    $stderr = original_stderr
+  end
+  fake.string
+end
+
+describe Dean::Cli::Application do
   describe "bump_version VALUE" do
     before(:each) do
       @temp_plist = setup_plist('1.2.3', '4.5.6')
+      stub_dean_yml(@temp_plist.path)
+    end
 
-      Dean::ConfigurationHelper.stub(:all_environments) {
-        { plist: @temp_plist }
-      }
+    after(:each) do
+      File.delete(@deanfile.path)
     end
 
     context "bump_version major" do
-      let(:output) { capture(:stdout) { subject.bump('major') } }
-
       it "bumps the major version" do
-        expect(Plist::parse_xml(@temp_plist.path)['CFBundleVersion']).to eq '2.2.3'
-        expect(Plist::parse_xml(@temp_plist.path)['CFBundleShortVersionString']).to eq '5.5.6'
+        subject.bump('major')
+        expect(Plist::parse_xml(@temp_plist.path)['CFBundleVersion']).to eq '2.0.0'
+        expect(Plist::parse_xml(@temp_plist.path)['CFBundleShortVersionString']).to eq '5.0.0'
       end
     end
 
     context "bump_version minor" do
-      let(:output) { capture(:stdout) { subject.bump('minor') } }
-
       it "bumps the major version" do
-        expect(Plist::parse_xml(@temp_plist.path)['CFBundleVersion']).to eq '1.3.3'
-        expect(Plist::parse_xml(@temp_plist.path)['CFBundleShortVersionString']).to eq '4.6.6'
+        subject.bump('minor')
+        expect(Plist::parse_xml(@temp_plist.path)['CFBundleVersion']).to eq '1.3.0'
+        expect(Plist::parse_xml(@temp_plist.path)['CFBundleShortVersionString']).to eq '4.6.0'
       end
     end
 
     context "bump_version patch" do
-      let(:output) { capture(:stdout) { subject.bump('patch') } }
-
       it "bumps the major version" do
+        subject.bump('patch')
         expect(Plist::parse_xml(@temp_plist.path)['CFBundleVersion']).to eq '1.2.4'
         expect(Plist::parse_xml(@temp_plist.path)['CFBundleShortVersionString']).to eq '4.5.7'
       end
